5cb2c750755d904b65f4a4d4c7a7490f57171998
Annika Nyholm <annika.nyholm@keylinx.se>
2025-01-07 11:47:54 +0100

    🔧 config global variables in scss

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,15 +4,12 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	// css: {
-	// 	preprocessorOptions: {
-	// 		scss: {
-	// 			additionalData: `
-    //       			@use 'src/styles/variables' as *;
-    //       			@use 'src/styles/mixins' as *;
-    //       			@use 'src/styles/base' as *;
-	// 			`,
-	// 		},
-	// 	},
-	// },
+	css: {
+		preprocessorOptions: {
+			scss: {
+				api: 'modern-compiler',
+				additionalData: `@use '/src/styles/utils' as *;`,
+			},
+		},
+	},
 });
