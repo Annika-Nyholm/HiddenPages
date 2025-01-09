b9545560d14a434221ed1a1e06b34f7657249114
Annika Nyholm <annika.nyholm@keylinx.se>
2025-01-09 18:22:42 +0100

    💄 fixed responsiveness. add TS to dynamicly change layout. adjusted scss in other files

--- client/src/pages/LayoutPage.tsx
+++ client/src/pages/LayoutPage.tsx
@@ -1,16 +1,20 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { Header } from '../components/Header';
 import { Footer } from '../components/Footer';
 import '../styles/pages/layoutPage.scss';
 
 export const LayoutPage = () => {
+	const location = useLocation();
+
+	const isHomePage = location.pathname === '/';
+
 	return (
-		<>
+		<div className={isHomePage ? 'home-layout' : 'app-layout'}>
 			<Header />
 			<main>
 				<Outlet></Outlet>
 			</main>
 			<Footer />
-		</>
+		</div>
 	);
 };
